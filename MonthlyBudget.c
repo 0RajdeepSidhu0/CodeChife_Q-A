3d2444da64e72b9c1eeb16e4f3e81057a68698c4
Rajdeep Sidhu <122351556+0RajdeepSidhu0@users.noreply.github.com>
2024-03-03 23:38:33 +0530

    Update MonthlyBudget.c

--- MonthlyBudget.c
+++ MonthlyBudget.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-/*Q: Akshat has X rupees to spend in the current month. His daily expenditure is 
+
+/*
+Q: Akshat has X rupees to spend in the current month. His daily expenditure is 
 Y rupees, i.e., he spends Y rupees each day. Given that the current month has 
 30 days, find out if Akshat has enough money to meet his daily expenditures for this month.
 */
