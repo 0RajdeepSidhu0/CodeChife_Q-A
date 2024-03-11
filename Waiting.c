94db8161dc57a7884293057f96b0d27247e7fc2f
Rajdeep Sidhu <122351556+0RajdeepSidhu0@users.noreply.github.com>
2024-03-11 22:57:48 +0530

    Update Waiting.c

--- Waiting.c
+++ Waiting.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /*
 Q: Chef is eagerly waiting for a piece of information. His secret agent told him that this information would be revealed to him after K weeks.
 X days have already passed and Chef is getting restless now. Find the number of remaining days Chef has to wait for, to get the information.
